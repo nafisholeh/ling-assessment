7a6644464184b73bf4f019f17e071c46889a9a9a
nafisholeh <rachmadnafisholeh@gmail.com>
2024-06-03 10:50:43 +0700

    feat(theme): setup ui-kitten for UI styling and theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
+import React from 'react';
 import { Stack } from 'expo-router';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
-}
+const RootStack: React.FC = () => (
+  <Stack>
+    <Stack.Screen name="index" />
+  </Stack>
+);
+
+const RootLayout: React.FC = () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <RootStack />
+    </ApplicationProvider>
+  </>
+);
+
+export default RootLayout;
